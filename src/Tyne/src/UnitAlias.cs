4d8130b4bff078a456b47e4c792c04f5baa918fb
Alex Noddings <alexnoddings@users.noreply.github.com>
2022-06-30 15:15:09 +0100

    Add `UnitAlias` to allow for `<Using Include="Tyne.UnitAlias" Static="true" />`

---
+++ src/Tyne/src/UnitAlias.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Tyne;
+
+public static class UnitAlias
+{
+	[SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Intentionally designed to allow `unit` as a type when statically used.")]
+	public static ref readonly Unit unit => ref Unit.Value;
+}
