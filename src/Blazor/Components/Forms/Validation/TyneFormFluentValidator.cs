411af583fdb5c9f5835c74f1d14841d5f59602c3
Alex Noddings <alexnoddings@users.noreply.github.com>
2023-05-16 21:24:11 +0100

    Support using validators directly injected by DI

--- src/Blazor/Components/Forms/Validation/TyneFormFluentValidator.cs
+++ src/Blazor/Components/Forms/Validation/TyneFormFluentValidator.cs
@@ -12,11 +12,34 @@ public sealed class TyneFormFluentValidator<TModel> : ComponentBase, IDisposable
     [CascadingParameter]
     private EditContext CurrentEditContext { get; set; } = null!;
 
-    [Parameter, EditorRequired]
-    public IEnumerable<IValidator<TModel>> Validators { get; set; } = null!;
-
     [Parameter]
     public FormValidationEvents ValidationEvents { get; set; } = FormValidationEvents.Default;
+
+    /// <summary>
+    ///     The <see cref="IValidator{T}"/>s to use for validating <typeparamref name="TModel"/>.
+    /// </summary>
+    /// <remarks>
+    ///     These are copied during component initialisation. Changing them afterwards is not supported.
+    /// </remarks>
+    [Parameter]
+    public IEnumerable<IValidator<TModel>>? Validators { get; set; }
+
+    /// <summary>
+    ///     <para>
+    ///         When <see langword="false"/>, only the validators specified by <see cref="Validators"/> will be used for validation.
+    ///     </para>
+    ///     <para>
+    ///         When <see langword="true"/>, <c>[Inject]</c>ed <see cref="IValidator{T}"/>s will also be used.
+    ///     </para>
+    /// </summary>
+    /// <remarks>
+    ///     Defaults to <see langword="true"/>.
+    /// </remarks>
+    [Parameter]
+    public bool UseInjected { get; set; } = true;
+
+    [Inject]
+    private IEnumerable<IValidator<TModel>>? InjectedValidators { get; init; }
 
     protected override void OnInitialized()
     {
@@ -27,11 +50,14 @@ public sealed class TyneFormFluentValidator<TModel> : ComponentBase, IDisposable
                  $"For example, you can use {nameof(TyneFormFluentValidator<TModel>)} inside an EditForm.");
         }
 
-        if (Validators == null)
-            throw new InvalidOperationException($"{nameof(TyneFormFluentValidator<TModel>)} requires a {nameof(Validators)} parameter.");
+        var validators = Enumerable.Empty<IValidator<TModel>>();
+        if (Validators is not null)
+            validators = validators.Union(Validators);
+        if (UseInjected && InjectedValidators is not null)
+            validators = validators.Union(InjectedValidators);
 
         _originalEditContext = CurrentEditContext;
-        _subscriptions = new TyneFormFluentValidatorSubscriptions<TModel>(CurrentEditContext, Validators)
+        _subscriptions = new TyneFormFluentValidatorSubscriptions<TModel>(CurrentEditContext, validators)
         {
             ValidationEvents = ValidationEvents
         };
