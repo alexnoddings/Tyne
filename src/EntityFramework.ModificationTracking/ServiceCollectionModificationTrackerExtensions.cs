bba12a2b7a16500323ab196cfff8c7f877f7738b
Alex Noddings <alexnoddings@users.noreply.github.com>
2023-07-09 19:02:05 +0100

    Support registering `DbContextModificationTracker` as any service lifetime

--- src/EntityFramework.ModificationTracking/ServiceCollectionModificationTrackerExtensions.cs
+++ src/EntityFramework.ModificationTracking/ServiceCollectionModificationTrackerExtensions.cs
@@ -5,14 +5,17 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionModificationTrackerExtensions
 {
-    public static TyneBuilder AddDbContextModificationTracker(this TyneBuilder builder)
+    public static TyneBuilder AddDbContextModificationTracker(this TyneBuilder builder) =>
+        AddDbContextModificationTracker(builder, ServiceLifetime.Scoped);
+
+    public static TyneBuilder AddDbContextModificationTracker(this TyneBuilder builder, ServiceLifetime serviceLifetime)
     {
         ArgumentNullException.ThrowIfNull(builder);
 
         if (!builder.Services.Any(serviceDescriptor => serviceDescriptor.ServiceType == typeof(ITyneUserService)))
             throw new InvalidOperationException($"Please ensure an {nameof(ITyneUserService)} is registered before calling {nameof(AddDbContextModificationTracker)}.");
 
-        builder.Services.AddScoped<IDbContextModificationTracker, DbContextModificationTracker>();
+        builder.Services.Add(new(typeof(IDbContextModificationTracker), typeof(DbContextModificationTracker), serviceLifetime));
 
         return builder;
     }
