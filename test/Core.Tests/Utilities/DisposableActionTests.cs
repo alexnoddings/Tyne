8c467c515a55d6ebca879c41b5b4712a52f16575
Alex Noddings <alexnoddings@users.noreply.github.com>
2023-09-02 10:58:00 +0100

    Tests for core utilities

---
+++ test/Core.Tests/Utilities/DisposableActionTests.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tyne;
+
+public class DisposableActionTests
+{
+    [Fact]
+    public void NullAction_Throws()
+    {
+        AssertExt.ThrowsArgumentNullException(() => new DisposableAction(null!));
+        AssertExt.ThrowsArgumentNullException(() => new DisposableAction(null!, true));
+        AssertExt.ThrowsArgumentNullException(() => new DisposableAction(null!, false));
+    }
+
+    [Fact]
+    [SuppressMessage("Reliability", "CA2000: Dispose objects before losing scope", Justification = "It is disposed.")]
+    [SuppressMessage("Major Code Smell", "S3966: Objects should not be disposed more than once", Justification = "Done for testing.")]
+    public void OnlyCallOnce_True_OnlyCallsOnce()
+    {
+        var action = Substitute.For<Action>();
+
+        var disposableAction = new DisposableAction(action, onlyCallOnce: true);
+
+        for (var i = 0; i < 10; i++)
+            disposableAction.Dispose();
+
+        action.Received(1).Invoke();
+    }
+
+    [Fact]
+    [SuppressMessage("Reliability", "CA2000: Dispose objects before losing scope", Justification = "It is disposed.")]
+    [SuppressMessage("Major Code Smell", "S3966: Objects should not be disposed more than once", Justification = "Done for testing.")]
+    public void OnlyCallOnce_False_CallsEachTime()
+    {
+        var action = Substitute.For<Action>();
+
+        var disposableAction = new DisposableAction(action, onlyCallOnce: false);
+
+        for (var i = 0; i < 10; i++)
+            disposableAction.Dispose();
+
+        action.Received(10).Invoke();
+    }
+
+    [Fact]
+    [SuppressMessage("Reliability", "CA2000: Dispose objects before losing scope", Justification = "It is disposed.")]
+    [SuppressMessage("Major Code Smell", "S3966: Objects should not be disposed more than once", Justification = "Done for testing.")]
+    public void OnlyCallOnce_Default_OnlyCallsOnce()
+    {
+        var action = Substitute.For<Action>();
+
+        var disposableAction = new DisposableAction(action);
+
+        for (var i = 0; i < 10; i++)
+            disposableAction.Dispose();
+
+        action.Received(1).Invoke();
+    }
+}
