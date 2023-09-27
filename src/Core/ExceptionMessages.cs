c224fb0d10f1173bf7b9f607f11e8a6dc8671d0c
Alex Noddings <alexnoddings@users.noreply.github.com>
2023-09-27 18:23:24 +0100

    Result unwrapping improvements

--- src/Core/ExceptionMessages.cs
+++ src/Core/ExceptionMessages.cs
@@ -8,6 +8,8 @@ internal static class ExceptionMessages
     private static readonly EmbeddedResourceManager Resources =
         EmbeddedResourceManager.GetFor(typeof(ExceptionMessages));
 
+    internal static readonly string ExceptionFactoryReturnedNull = Resources.GetMemberString(culture: default);
+
     internal static string JsonConversionForTypeNotSupported(Type type) => Resources.GetMemberString(culture: default, arg0: type.Name);
     internal static readonly string JsonConverterFactoryCouldNotCreateConverter = Resources.GetMemberString(culture: default);
 
@@ -16,7 +18,7 @@ internal static class ExceptionMessages
     internal static readonly string Option_NoneHasNoValue = Resources.GetMemberString(culture: default);
     internal static readonly string Option_SomeMustHaveValue = Resources.GetMemberString(culture: default);
 
-    internal static readonly string Result_CannotUnwrapError = Resources.GetMemberString(culture: default);
+    internal static readonly string Result_CannotUnwrapValueFromError = Resources.GetMemberString(culture: default);
     internal static readonly string Result_Invalid = Resources.GetMemberString(culture: default);
     internal static readonly string Result_ErrorHasNoValue = Resources.GetMemberString(culture: default);
     internal static readonly string Result_ErrorMustHaveError = Resources.GetMemberString(culture: default);
