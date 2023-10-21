9a6c84b4a0a5771af494e87b23d8f3ae40b5a832
Alex Noddings <alexnoddings@users.noreply.github.com>
2023-10-21 13:34:30 +0100

    Add new filtered table columns

---
+++ src/Blazor/Components/TableV2/Columns/TyneColumnPopoverHeader.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Tyne.Blazor.Tables.Columns;
+
+/// <inheritdoc/>
+public partial class TyneColumnPopoverHeader<TResponse> : TyneColumnPopoverHeaderBase<TResponse>
+{
+    /// <summary>
+    ///     The icon to use for the popover <see cref="Content"/> control.
+    /// </summary>
+    /// <remarks>
+    ///     Leave this as <see cref="string.Empty"/> (or do not supply a parameter)
+    ///     to use the default icons which change based on
+    ///     <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsActive"/>
+    ///     and <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsOpen"/>.
+    /// </remarks>
+    [Parameter]
+    public string Icon { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     The icon to use for the popover <see cref="Content"/> control.
+    /// </summary>
+    /// <remarks>
+    ///     Uses the <see cref="Icon"/> if set,
+    ///     otherwise uses defaults based on 
+    ///     <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsActive"/>
+    ///     and <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsOpen"/>.
+    /// </remarks>
+    protected override string FilterIcon
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(Icon))
+                return Icon;
+
+            if (IsActive)
+                return Icons.Material.Filled.SavedSearch;
+
+            if (IsOpen)
+                return Icons.Material.Filled.SearchOff;
+
+            return Icons.Material.Filled.Search;
+        }
+    }
+
+    /// <summary>
+    ///     The colour of the <see cref="Icon"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Leave as <see langword="null"/> (or do not supply a parameter)
+    ///     to use the default colours which change based on
+    ///     <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsActive"/>
+    ///     and <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsOpen"/>.
+    /// </remarks>
+    [Parameter]
+    public Color? IconColour { get; set; }
+
+    /// <summary>
+    ///     The colour to use for the <see cref="Icon"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Uses the <see cref="IconColour"/> if set,
+    ///     otherwise uses defaults based on 
+    ///     <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsActive"/>
+    ///     and <see cref="TyneColumnPopoverHeaderBase{TResponse}.IsOpen"/>.
+    /// </remarks>
+    protected override Color FilterIconColour
+    {
+        get
+        {
+            if (IconColour is not null)
+                return IconColour.Value;
+
+            if (IsActive)
+                return Color.Primary;
+
+            return Color.Default;
+        }
+    }
+}
---
+++ src/Blazor/Components/TableV2/Columns/TyneColumnPopoverHeaderBase.razor.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Tyne.Blazor.Tables.Columns;
+
+/// <summary>
+///     Renders a <see cref="MudTh"/> containing <see cref="Header"/>, which has a button to open a popover with <see cref="Content"/> in.
+/// </summary>
+/// <typeparam name="TRequest">The type of request which loads data in the context.</typeparam>
+public abstract partial class TyneColumnPopoverHeaderBase<TResponse>
+{
+    /// <summary>
+    ///     <see langword="true"/> if the column is being actively filtered; otherwise, <see langword="false"/>.
+    /// </summary>
+    [Parameter]
+    public bool IsActive { get; set; }
+
+    /// <summary>
+    ///     <see langword="true"/> if the <see cref="Content"/> should be shown; otherwise, <see langword="false"/>.
+    /// </summary>
+    [Parameter]
+    public bool IsOpen { get; set; }
+
+    /// <summary>
+    ///     Invoked when the user tries to close the <see cref="Content"/>.
+    /// </summary>
+    [Parameter]
+    public EventCallback<bool> IsOpenChanged { get; set; }
+
+    /// <summary>
+    ///     Which property on <typeparamref name="TResponse"/> to order by when the column is interacted with.
+    /// </summary>
+    /// <remarks>
+    ///     Use <see cref="TyneKey.Empty"/> (or do not supply a parameter) to disable ordering on this column header.
+    /// </remarks>
+    [Parameter]
+    public TyneKey OrderBy { get; set; }
+
+    /// <summary>
+    ///     The icon to use for the <see cref="Content"/> control.
+    /// </summary>
+    protected abstract string FilterIcon { get; }
+
+
+    /// <summary>
+    ///     The colour of the <see cref="FilterIcon"/>.
+    /// </summary>
+    protected abstract Color FilterIconColour { get; }
+
+    /// <summary>
+    ///     A <see cref="Func{TResult}"/> which is invoked when the 'clear value' icon is clicked.
+    /// </summary>
+    /// <remarks>
+    ///     Leave as <see langword="null"/> to disable the 'clear value' icon button.
+    /// </remarks>
+    [Parameter]
+    public Func<Task>? ClearValue { get; set; }
+
+    /// <summary>
+    ///     The <see cref="RenderFragment"/> to display in the column header.
+    /// </summary>
+    /// <remarks>
+    ///     This content is always shown.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment? Header { get; set; }
+
+    /// <summary>
+    ///     The <see cref="RenderFragment"/> to display inside the popover.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? Content { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        // If no IsOpenChanged is passed, then the popover will
+        // never open as invoking the callback won't do anything.
+        // So if no delegate is provided, use a default which simply sets IsOpen.
+        if (!IsOpenChanged.HasDelegate)
+            IsOpenChanged = EventCallback.Factory.Create<bool>(this, isOpen => IsOpen = isOpen);
+    }
+}
