392d0853f2235051a4488703dd62d831ee1b2952
Alex Noddings <alexnoddings@users.noreply.github.com>
2024-04-10 21:59:30 +0100

    Add not found endpoint tests

--- test/AspNetCore.IntegrationTests.TestWebApp/TestWebAppHost.cs
+++ test/AspNetCore.IntegrationTests.TestWebApp/TestWebAppHost.cs
@@ -1,19 +1,36 @@
 using System.Diagnostics.CodeAnalysis;
 
-namespace Tyne.AspNetCore.TestApp;
+namespace Tyne.AspNetCore;
 
 [SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "It's not a utility class.")]
 public sealed class TestWebAppHost
 {
+    public const string NotFoundSyncHandlerHeaderKey = "NotFoundSyncHeader";
+    public const string NotFoundAsyncHandlerBodyMessage = "Not found async handler content";
+
+    public const int InvalidTestRequestStatusCode = 418;
+    public const string InvalidTestRequestBodyMessage = "invalid test request";
+
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddProblemDetails();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
 
         using var app = builder.Build();
 
         app.UseRouting();
+
+        app.MapFallbackToNotFound("/test/notfound/default/{**_}");
+        app.MapFallbackToNotFound("/test/notfound/sync-handler/{**_}", httpContext => httpContext.Response.Headers.Append(NotFoundSyncHandlerHeaderKey, "true"));
+        app.MapFallbackToNotFound("/test/notfound/async-handler/{**_}", async httpContext => await httpContext.Response.WriteAsync(NotFoundAsyncHandlerBodyMessage));
+
+        app.MapFallback("{**_}", async httpContext =>
+        {
+            httpContext.Response.StatusCode = InvalidTestRequestStatusCode;
+            await httpContext.Response.WriteAsync(InvalidTestRequestBodyMessage);
+        });
 
         app.Run();
     }
---
+++ test/AspNetCore.IntegrationTests/TestWebAppCollection.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tyne.AspNetCore;
+
+[CollectionDefinition(Name)]
+[SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "This is an xUnit convention.")]
+public class TestWebAppCollection : ICollectionFixture<TestWebAppFactory>
+{
+    public const string Name = nameof(TestWebAppCollection);
+}
