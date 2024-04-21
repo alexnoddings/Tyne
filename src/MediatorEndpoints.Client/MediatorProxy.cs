3f0ba41e56dae1b74dd98180b8443ac02e93a590
Alex Noddings <alexnoddings@users.noreply.github.com>
2024-04-21 21:20:00 +0100

    Better MediatorEndpoints client exception message #140

--- src/MediatorEndpoints.Client/MediatorProxy.cs
+++ src/MediatorEndpoints.Client/MediatorProxy.cs
@@ -14,7 +14,6 @@ internal class MediatorProxy : IMediatorProxy
     public async Task<TResponse> Send<TResponse>(IApiRequest<TResponse> request, CancellationToken cancellationToken = default)
     {
         var result = await _mediator.SendAsync(request, cancellationToken).ConfigureAwait(false);
-        // This is what the old MediatorEndpoints HttpMediatorProxy would throw if the Response was null
-        return result.Unwrap(() => new InvalidOperationException($"Could not read response. De-serialising returned null, not {nameof(TResponse)}."));
+        return result.Unwrap(() => $"Error sending request {request?.GetType().Name}.");
     }
 }
