ae9dc6f708d52766bdd8758fa1c910f5ffac15b6
Alex Noddings <alexnoddings@users.noreply.github.com>
2025-03-19 20:02:58 +0000

    Remove core search type

--- src/Tyne.Core/Searching/ISearchQuery.cs
+++
@@ -1,8 +0,0 @@
-namespace Tyne.Searching;
-
-/// <summary>
-///		A search query. Provides pagination and ordering by default.
-/// </summary>
-public interface ISearchQuery : ISearchQueryPage, ISearchQueryOrder
-{
-}
